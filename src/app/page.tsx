6521ee7dbd81030f36d6a48beef1857b3aa791a2
Anirban Banerjee <abanerjee763@gmail.com>
2025-04-12 22:27:56 +0530

    Installed shadcn ui  for design components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl">Hello world</h1>
-      <h1 className="text-3xl font-barlow">Hello world</h1></div>
+      <h1 className="text-3xl font-barlow">Hello world</h1><Button variant='destructive'>CLick here</Button></div>
+      
   );
 }
